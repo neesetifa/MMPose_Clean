3b4106d8a3acb96655353d9228d148d3bb493831
neesetifa <36785948+neesetifa@users.noreply.github.com>
2024-02-18 10:35:53 +0800

    Create __init__.py

---
+++ evaluation/metrics/__init__.py
@@ -0,0 +1,6 @@
+from .coco_metric import CocoMetric
+# from .keypoint_2d_metrics import (AUC, EPE, NME, JhmdbPCKAccuracy,
+#                                   MpiiPCKAccuracy, PCKAccuracy)
+
+__all__ = ['CocoMetric',]
+# 'PCKAccuracy', 'MpiiPCKAccuracy', 'JhmdbPCKAccuracy', 'AUC', 'EPE', 'NME',
