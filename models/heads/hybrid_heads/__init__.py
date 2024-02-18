032ea69aee9bd6961bdc79321d4e3f1fdf52c0b0
neesetifa <36785948+neesetifa@users.noreply.github.com>
2024-02-18 10:41:18 +0800

    Create __init__.py

---
+++ models/heads/hybrid_heads/__init__.py
@@ -0,0 +1,6 @@
+# from .dekr_head import DEKRHead
+# from .vis_head import VisPredictHead
+# from .yoloxpose_head import YOLOXPoseHead
+from .simcc_rle_head import SimCCRLEHead
+
+__all__ = ['SimCCRLEHead']  # 'DEKRHead', 'VisPredictHead', 'YOLOXPoseHead', 
