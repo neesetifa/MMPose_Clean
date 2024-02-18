e92ad203d32f0afb08de9ea67834d5efb9a6e224
neesetifa <36785948+neesetifa@users.noreply.github.com>
2024-02-18 10:42:05 +0800

    Create __init__.py

---
+++ models/heads/regression_heads/__init__.py
@@ -0,0 +1,8 @@
+from .dsnt_head import DSNTHead
+from .integral_regression_head import IntegralRegressionHead
+from .regression_head import RegressionHead
+from .rle_head import RLEHead
+
+__all__ = ['RegressionHead', 'IntegralRegressionHead',
+           'DSNTHead', 'RLEHead',
+           ]
